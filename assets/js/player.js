962f04799cfc454a4a988076929d57c3d479ee53
anilexsan <72182890+anilexsan@users.noreply.github.com>
2020-12-12 03:30:16 -0300

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -381,7 +381,7 @@ window.addEventListener("message", function (e) {
 			jwplayer().on('error', function (e) {
 				if (e.code == 232011) {
 					jwplayer().load({
-						file: "https://i.imgur.com/gC9t74z.png"
+						file: "https://i.imgur.com/5GEkUDs.mp4"
 					});
 					jwplayer().setControls(false);
 					jwplayer().setConfig({
